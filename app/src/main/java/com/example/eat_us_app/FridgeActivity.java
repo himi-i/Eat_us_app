7335ddc42eb9eaa292b8af94e40797952970e2ee
mooniswan <mcw1156@naver.com>
2023-11-29 09:40:40 +0900

    [feat] AlertDialog in addfridge View 설정 완료

--- app/src/main/java/com/example/eat_us_app/FridgeActivity.java
+++ app/src/main/java/com/example/eat_us_app/FridgeActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,9 +16,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class FridgeActivity extends AppCompatActivity {
-    /* private RecyclerView mRecyclerView;
-    private RecyclerView.Adapter mAdapter;
-    private RecyclerView.LayoutManager mLayoutManager; */
+    private ArrayList<Item> itemList = new ArrayList<>();
+    private RecyclerView fridgeRecyclerView;
+    private RecyclerViewAdapter fridgeAdapter;
     Button addfridge_btn;
 
     @Override
@@ -32,5 +34,36 @@ public class FridgeActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        /*
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new RecyclerViewAdapter(itemList);
+        recyclerView.setAdapter(adapter);
+
+        EditText editTextName = findViewById(R.id.editTextName);
+        EditText editTextQuantity = findViewById(R.id.editTextQuantity);
+        EditText editTextExpirationDate = findViewById(R.id.editTextExpirationDate);
+        Button buttonSave = findViewById(R.id.buttonSave);
+
+        buttonSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = editTextName.getText().toString();
+                int quantity = Integer.parseInt(editTextQuantity.getText().toString());
+                String expirationDate = editTextExpirationDate.getText().toString();
+
+                // Add item to list and notify adapter
+                itemList.add(new Item(name, quantity, expirationDate));
+                adapter.notifyDataSetChanged();
+
+                // Clear input fields
+                editTextName.setText("");
+                editTextQuantity.setText("");
+                editTextExpirationDate.setText("");
+            }
+        });
+    } */
+
     }
 }
+
